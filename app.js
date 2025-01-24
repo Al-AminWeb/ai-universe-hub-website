1d30e8bd271e890496e243d25bf3ea61565bd398
Al-AminWeb <143194944+Al-AminWeb@users.noreply.github.com>
2025-01-25 00:33:08 +0600

    search feature adding

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const loadTool = async () =>{
-    const res = await fetch('https://openapi.programming-hero.com/api/ai/tools');
+const loadTool = async (searchText) => {
+    const res = await fetch(` https://openapi.programming-hero.com/api/ai/tool/${searchText}`);
     const data = await res.json();
     const tools = data.data.tools;
     displayTool(tools);
 }
 
 
-const displayTool = tools =>{
+const displayTool = tools => {
 
     const toolContainer = document.getElementById('tool-container');
 
-    tools.forEach(tool =>{
-       const toolCard = document.createElement("div");
-       toolCard.classList = `card bg-gray-300 w-96 shadow-xl`;
-       toolCard.innerHTML = `
+    tools.forEach(tool => {
+        const toolCard = document.createElement("div");
+        toolCard.classList = `card bg-gray-300 p-4 mt-10 shadow-xl`;
+        toolCard.innerHTML = `
        <figure>
                     <img
                             src="${tool.image}"
@@ -21,15 +21,30 @@ const displayTool = tools =>{
                 </figure>
                 <div class="card-body">
                     <h2 class="card-title">${tool.name}</h2>
-                    <p></p>
+                    <div>
+                    <h6 class="font-extrabold">Features</h6>
+                    <ol class="list-decimal ml-5">
+                    <li>Natural language processing</li>
+                    <li>Contextual understanding</li>
+                    <li>Text generation</li>
+                    </ol>
+                    </div>
                     <div class="card-actions justify-center">
-                        <button class="btn btn-primary">Buy Now</button>
+                        <button class="btn btn-primary">See More</button>
                     </div>
                 </div>
        `;
         toolContainer.appendChild(toolCard);
 
     })
+}
+
+const handleSearch = () => {
+ const searchField = document.getElementById('search-field');
+ const searchText = searchField.value;
+ console.log(searchText);
+
 
 }
-loadTool();
+
+// loadTool();
